54b1ed6fbd05a83881d9330507e76fee8098987b
Qiū Ruì <rexarski@gmail.com>
2021-09-14 16:50:45 -0400

    Update cron-job.r
    Simplify the coding

--- reddit-nba-scraper/cron-job.r
+++ reddit-nba-scraper/cron-job.r
@@ -1,12 +1,7 @@
-library(tidyverse)
-library(httr)
-library(jsonlite)
 library(RedditExtractoR)
 
 top_nba_daily <- find_thread_urls(subreddit = "nba", sort_by = "top", period = "day")
 str(top_nba_daily)
 threads_contents <- get_thread_content(top_nba_daily$url[1:10]) # time-consuming
-str(threads_contents$threads) # thread metadata
-str(threads_contents$comments)
 
 save(threads_contents, file = paste0("data/nba-reddit/", Sys.Date(), "-top10.RDS"))
