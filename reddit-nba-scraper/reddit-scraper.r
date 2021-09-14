8181e93b2b4d859247567df334a0e16bc60feb16
Qiū Ruì <rexarski@gmail.com>
2021-09-14 12:56:30 -0400

    Update reddit scraper
    Add new script for cron job

--- reddit-nba-scraper/reddit-scraper.r
+++ reddit-nba-scraper/reddit-scraper.r
@@ -33,8 +33,8 @@ result_content
 
 top_nba_daily <- find_thread_urls(subreddit = "nba", sort_by = "top", period = "day")
 str(top_nba_daily)
-threads_contents <- get_thread_content(top_nba_daily$url[1:2]) # for the sake of simplicity
+threads_contents <- get_thread_content(top_nba_daily$url[1:10]) # time-consuming
 str(threads_contents$threads) # thread metadata
 str(threads_contents$comments)
 
-save(threads_contents, file = paste0("data/nba-reddit/", Sys.Date(), "-top100.RDS"))
+save(threads_contents, file = paste0("data/nba-reddit/", Sys.Date(), "-top10.RDS"))
