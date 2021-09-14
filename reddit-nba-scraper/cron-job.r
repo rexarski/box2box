4983b15cb8ef9946c8e2c07c6075c65147472773
Qiū Ruì <rexarski@gmail.com>
2021-09-14 17:49:55 -0400

    Update cron-job.r
    Change path of saved .RDS

--- reddit-nba-scraper/cron-job.r
+++ reddit-nba-scraper/cron-job.r
@@ -4,4 +4,4 @@ top_nba_daily <- find_thread_urls(subreddit = "nba", sort_by = "top", period = "
 str(top_nba_daily)
 threads_contents <- get_thread_content(top_nba_daily$url[1:10]) # time-consuming
 
-save(threads_contents, file = paste0(Sys.Date(), "-top10.RDS"))
+save(threads_contents, file = paste0("data/nba-reddit/", Sys.Date(), "-top10.RDS"))
